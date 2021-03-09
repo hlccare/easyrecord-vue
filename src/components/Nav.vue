6a4686a320a1ad887ac8e6fa08d537ebeba1a50d
hlccare <280108904@qq.com>
2021-03-09 20:15:55 +0800

    Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,19 @@
 import Nav from '@/components/Nav.vue';
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money" />
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label" />
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="statistic" />
+    </router-link>
   </div>
 </template>
 
-<script>
+<script lang='ts'>
 export default {};
 </script>
 
