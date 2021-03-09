6d2b4e65d4e9c403744e4dc422ed9932b4386477
hlccare <280108904@qq.com>
2021-03-09 16:35:17 +0800

    添加404处理及页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>404 - 当前访问页面不存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
