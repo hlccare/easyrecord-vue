1904ea0252fe20f2a52d3cbacae2fdfbe07da7d9
hlccare <280108904@qq.com>
2021-03-10 10:32:38 +0800

    修改svg fil属性，修改meta

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 import Nav from '@/components/Nav.vue';
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money" />
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label" />
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistic" />
       统计
     </router-link>
@@ -36,6 +36,9 @@ nav {
       height: 32px;
       width: 32px;
     }
+  }
+  > .item.selected {
+    color: red;
   }
 }
 </style>
