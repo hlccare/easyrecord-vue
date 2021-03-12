5cd58d268efcb55f9e3f738cebd177bf2ea1601e
hlccare <280108904@qq.com>
2021-03-12 10:04:02 +0800

    修改svgo-loader版本

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class Types extends Vue {
+  type = "-"; //'-'表示指出，‘+’表示收入
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      //type只能是'+'或'-'
+      throw new Error("type is unknown");
+    }
+    this.type = type;
+  }
+}
+// export default {
+//   name: "Types",
+//   props: ["xxx"],
+//   data() {
+//     return {
+//       type: "-", //'-'表示指出，‘+’表示收入
+//     };
+//   },
+//   mounted() {
+//     console.log(this.xxx);
+//   },
+//   methods: {
+//     selectType(type) {
+//       if (type !== "-" && type !== "+") { //type只能是'+'或'-'
+//         throw new Error("type is unknown");
+//       }
+//       this.type = type;
+//     },
+//   },
+// };
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
