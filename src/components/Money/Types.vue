c9ca876553b0b5e04ef17e2134deab1b3ca600da
hlccare <280108904@qq.com>
2021-03-12 16:57:48 +0800

    完成money的Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -9,15 +9,15 @@
 
 <script lang='ts'>
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
-@Component({
-  props: {
-    propMessage: String,
-  },
-})
+import { Component, Prop } from "vue-property-decorator";
+
+// 1 自动提示更智能
+// 2 不能随意调用方法
+// 编译时会报错，无法变成JS，严谨
+@Component
 export default class Types extends Vue {
   type = "-"; //'-'表示指出，‘+’表示收入
-  helloMsg = "Hello," + this.propMessage;
+
   selectType(type: string) {
     if (type !== "-" && type !== "+") {
       //type只能是'+'或'-'
@@ -26,26 +26,6 @@ export default class Types extends Vue {
     this.type = type;
   }
 }
-// export default {
-//   name: "Types",
-//   props: ["xxx"],
-//   data() {
-//     return {
-//       type: "-", //'-'表示指出，‘+’表示收入
-//     };
-//   },
-//   mounted() {
-//     console.log(this.xxx);
-//   },
-//   methods: {
-//     selectType(type) {
-//       if (type !== "-" && type !== "+") { //type只能是'+'或'-'
-//         throw new Error("type is unknown");
-//       }
-//       this.type = type;
-//     },
-//   },
-// };
 </script>
 
 <style lang="scss" scoped>
