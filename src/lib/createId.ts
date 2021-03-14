cc68465417df736840e55468d41b373ed7faee31
hlccare <280108904@qq.com>
2021-03-14 21:01:05 +0800

    修改createId.js,将最后的id存储4

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,7 +1,9 @@
-let id: number = parseInt(window.localStorage.getItem("_idMax") || '0') || 0
+const key = "_idMax"
+let id: number = parseInt(window.localStorage.getItem(key) || '0') || 0
 
 function createId(){
     id++
+    window.localStorage.setItem(key, id.toString())
     return id
 
 }
