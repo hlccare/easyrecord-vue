1eb21ece501737a381cc5446bb0b9f77a73cb757
hlccare <280108904@qq.com>
2021-03-14 11:52:37 +0800

    新建models文件夹，添加rightsvg图片，完成Labels组件的添加功能

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,23 +17,28 @@ import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Notes from "@/components/Money/Notes.vue";
 import { Component, Watch } from "vue-property-decorator";
-import { model } from "@/model";
+import { recordListModel } from "@/models/recordListModel";
+import { tagListModel } from "@/models/tagListModel";
+
 
 // const model = require("@/model.js").model; //在TS中引入JS
 // const {model} = require("@/model.js");
+
+const recordList = recordListModel.fetch()
+const tagList = tagListModel.fetch()
 
 @Component({
   components: { NumberPad, Types, Tags, Notes },
 })
 export default class Money extends Vue {
   name = "Money";
-  tags = ["衣", "食", "住", "行"];
-  recordList = model.fetch();
+  tags = tagList;
+  recordList = recordList;
   record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
 
   @Watch("recordList")
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 
   OnUpdateTags(value: string[]) {
@@ -53,7 +58,7 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    const record2: RecordItem = model.clone(this.record);
+    const record2: RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
