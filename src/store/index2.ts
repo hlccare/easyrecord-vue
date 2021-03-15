b9d8993f85b6148c914ea6718eda4171a40794a9
hlccare <280108904@qq.com>
2021-03-15 16:50:06 +0800

    模块化store，删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,34 +1,10 @@
-import { recordListModel } from "@/models/recordListModel";
-import { tagListModel } from "@/models/tagListModel";
+import recordStore from "./recordStore";
+import tagStore from "./tagStore";
+
 
 const store = {
-    //1. 全局变量过多
-    //2. 严重依赖window
-
-    //record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => {
-        recordListModel.create(record)
-    },
-    //tag store
-    tagList: tagListModel.fetch(),
-    findTag(id: string) {
-        return this.tagList.filter((t) => t.id === id)[0]; //返回的是数组，所以[0]
-    },
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-        if (message === "duplicated") {
-            window.alert("标签名重复了");
-        } else if (message === "success") {
-            window.alert("添加成功");
-        }
-    },
-    removeTag: (id: string) => {
-        return tagListModel.remove(id)
-    },
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name)
-    }
+    ...recordStore,
+    ...tagStore
 }
 
 export default store;
