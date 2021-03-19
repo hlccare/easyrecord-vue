a31a46b02219ec8f43fd5f77d433443355f00b52
hlccare <280108904@qq.com>
2021-03-20 00:54:03 +0800

    修复默认添加tag提示的bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,6 +1,10 @@
 import Component from "vue-class-component";
 import Vue from "vue";
 
+const map: {[key: string]: string} ={
+    'tag name duplicated':'标签名重复了'
+}
+
 @Component
 class TagHelper extends Vue {
     createTag() {
@@ -9,6 +13,9 @@ class TagHelper extends Vue {
             this.$store.commit("createTag", name);
         } else {
             return window.alert("标签名不可为空");
+        }
+        if(this.$store.state.createTagError){
+            window.alert(map[this.$store.state.createTagError.message]||'未知错误')
         }
     }
 }
